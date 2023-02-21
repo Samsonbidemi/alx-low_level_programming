f74178642e87f05a2dc66bdb424bef6be93c6094
Samson Bidemi <bsamsonolarinre@gmail.com>
2023-02-21 14:12:00 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alway 0 (Sucess)
+ */
+
+int main(void)
+{
+	char *p = "_putchar\n";
+
+	while (*p)
+	{
+		_putchar(*p);
+		p++;
+	}
+	return (0);
+}
