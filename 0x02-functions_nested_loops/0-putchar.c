a19d80a3ffd4c67f63c1da7637935462374cacc3
Samson Bidemi <bsamsonolarinre@gmail.com>
2023-02-22 03:27:13 -0800

    Using islower

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,22 @@
-#!/bin/bash
 #include "main.h"
-/**
- * main - Entry point
- *
- * Return: Alway 0 (Sucess)
- */
 
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	char *_ = "_putchar\n";
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	while (*_)
-	{
-		_putchar(*_);
-		_++;
-	}
 	return (0);
 }
+
