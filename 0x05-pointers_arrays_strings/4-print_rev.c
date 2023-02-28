586ee8506d25a1b4c671b8c7f90291b38823422c
Samson Bidemi <bsamsonolarinre@gmail.com>
2023-02-28 05:33:44 -0800

    Reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,26 +1,19 @@
 include "main.h"
 
 /**
- * print_rev - prints a string in reverse, followed by a new line
- * @s: string to be printed
+ * print_rev - This function prints a reverse string
+ *
+ * @s: This is the input string
  */
 void print_rev(char *s)
 {
-	int i, j, len;
+	int index;
 
-	i = 0;
-
-	while (s[i] != '\0')
+	for (index = 0; s[index] != '\0'; index++)
+		;
+	for (index = index - 1; s[index] != '\0'; index--)
 	{
-		i++;
+		_putchar(s[index]);
 	}
-
-	len = i;
-
-	for (j = len - 1; j >= 0; j--)
-	{
-		_putchar(s[j]);
-	}
-
 	_putchar('\n');
 }
